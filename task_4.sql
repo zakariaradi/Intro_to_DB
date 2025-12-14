a5acb83198a232448efa067c393ace5521fc228b
zakaria.r <zakariaradi.dev@outlook.com>
2025-12-14 18:10:37 +0100

    Insert initial data into customer table

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,6 @@
 USE alx_book_store;
 
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'Books';
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
 
